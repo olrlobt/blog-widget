b6ffbb243c2a15859f4842c341ffb597cb0211ee
olrlobt <317tmdgjs@naver.com>
2024-03-09 18:36:46 +0900

    Initial commit

---
+++ src/main/java/olrlobt/githubtistoryposting/GithubTistoryPostingApplication.java
@@ -0,0 +1,13 @@
+package olrlobt.githubtistoryposting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTistoryPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTistoryPostingApplication.class, args);
+	}
+}
+
---
+++ src/test/java/olrlobt/githubtistoryposting/GithubTistoryPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package olrlobt.githubtistoryposting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTistoryPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
