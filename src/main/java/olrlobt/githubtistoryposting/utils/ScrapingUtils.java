13c27d8fc2dc9457ce7721acc11e5be7cfd88770
olrlobt <317tmdgjs@naver.com>
2024-10-01 21:41:10 +0900

    refactor: Scraping OkHttp 고성능 통신방식으로 개선

--- src/main/java/olrlobt/githubtistoryposting/utils/ScrapingUtils.java
+++ src/main/java/olrlobt/githubtistoryposting/utils/ScrapingUtils.java
@@ -2,15 +2,36 @@ package olrlobt.githubtistoryposting.utils;
 
 import java.io.IOException;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy(false)
 public class ScrapingUtils {
+	private final OkHttpClient client;
+
+	public ScrapingUtils() {
+		this.client = new OkHttpClient.Builder()
+				.retryOnConnectionFailure(true) // 연결 실패 시 재시도
+				.build();
+	}
+
 	@Cacheable(cacheNames = "url", key = "#url", sync = true)
 	public Document byUrl(String url) throws IOException {
-		return Jsoup.connect(url).get();
+		Request request = new Request.Builder().url(url).build();
+
+		try (Response response = client.newCall(request).execute()) {
+			if (response.isSuccessful() && response.body() != null) {
+				return Jsoup.parse(response.body().string());
+			}
+		}
+		return null;
 	}
 }
+
