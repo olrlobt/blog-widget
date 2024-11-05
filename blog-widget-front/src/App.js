e6976694fbcb7c68ad57543c8c71bf17fef24023
olrlobt <317tmdgjs@naver.com>
2024-11-05 15:05:05 +0900

    feat: 소개 페이지 UI 개선, 목차 추가

--- blog-widget-front/src/App.js
+++ blog-widget-front/src/App.js
@@ -1,30 +1,38 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
+import Content from './components/Content';
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
-  font-family: Arial, sans-serif;
+    font-family: Arial, sans-serif;
+    background-color: #f3f5f7; /* 메인 배경색 설정 */
+    min-height: 100vh; /* 화면 전체를 채우기 위해 최소 높이 설정 */
 `;
 
 const MainContainer = styled.div`
-  display: flex;
+    display: flex;
+`;
+
+
+const ContentContainer = styled(Content)`
+    flex: 3; // Content가 3/4 정도 차지
+    padding: 20px;
 `;
 
 function App() {
-  return (
-      <Router>
-        <AppContainer>
-          <Navbar />
-          <MainContainer>
-            <Sidebar />
-            <Routes>
-            </Routes>
-          </MainContainer>
-        </AppContainer>
-      </Router>
-  );
+    return (
+        <Router>
+            <AppContainer>
+                <Navbar />
+                <MainContainer>
+                    <Routes>
+                        <Route path="/" element={<ContentContainer />} />
+                    </Routes>
+                </MainContainer>
+            </AppContainer>
+        </Router>
+    );
 }
 
 export default App;
--- blog-widget-front/src/components/Navbar.js
+++ blog-widget-front/src/components/Navbar.js
@@ -1,36 +1,88 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaGithub } from 'react-icons/fa';
+import { SiTistory } from 'react-icons/si';
 
 const NavbarContainer = styled.nav`
-  background-color: #24292e;
-  color: white;
-  padding: 10px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+    background-color: #ffffff;
+    color: black;
+    padding: 10px 0;
+    height: 65px;
+    border-bottom: solid 1px #e9ebed;
+    display: flex;
+    justify-content: center;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.05); /* 그림자 추가 */
+`;
+
+const NavbarContent = styled.div`
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 1220px; /* 가운데에 고정된 너비 */
 `;
 
 const Title = styled.h1`
-  font-size: 1.5em;
-  margin: 0;
+    font-size: 1.5em;
+    margin: 0;
+`;
+
+const LinksContainer = styled.div`
+    display: flex;
+    align-items: center;
 `;
 
 const Links = styled.div`
-  a {
-    color: white;
-    margin-left: 15px;
-    text-decoration: none;
-  }
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+
+    a {
+        color: black;
+        margin-left: 15px;
+        text-decoration: none;
+    }
+`;
+
+const IconLinks = styled.div`
+    display: flex;
+    align-items: center;
+
+    a {
+        color: black;
+        margin-left: 10px;
+    }
+
+    .github-icon {
+        font-size: 1.4em; /* GitHub 아이콘 크기 증가 */
+    }
+
+    .tistory-icon {
+        font-size: 1.1em; /* Tistory 아이콘 기본 크기 */
+    }
 `;
 
 function Navbar() {
     return (
         <NavbarContainer>
-            <Title>My Docs</Title>
-            <Links>
-                <a href="/">Home</a>
-                <a href="/docs">Docs</a>
-            </Links>
+            <NavbarContent>
+                <LinksContainer>
+                    <Title>Blog Widget</Title>
+                    <Links>
+                        <a href="/about">소개</a>
+                        <a href="/get-started">API 시작하기</a>
+                        <a href="/examples">세트 예제</a>
+                    </Links>
+                </LinksContainer>
+                <IconLinks>
+                    <a href="https://github.com/olrlobt" target="_blank" rel="noopener noreferrer" className="github-icon">
+                        <FaGithub />
+                    </a>
+                    <a href="https://olrlobt.tistory.com" target="_blank" rel="noopener noreferrer" className="tistory-icon">
+                        <SiTistory />
+                    </a>
+                </IconLinks>
+            </NavbarContent>
         </NavbarContainer>
     );
 }
